d4c84f62c38b5791b036be8b1fa3af0f7eae482b
Manij Shrestha <manijshrestha@gmail.com>
2013-10-25 10:30:38 -0500

    Initial Commit

---
+++ src/com/example/viewflipperdynamic/DynamicViewFlipper.java
@@ -0,0 +1,59 @@
+package com.example.viewflipperdynamic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+
+public class DynamicViewFlipper extends Activity {
+
+	/**
+	 * List of Image URL that will populate the ViewFlipper
+	 */
+	private List<String> imageURLs = Arrays.asList(new String[] { "http://manijshrestha.github.io/android-icon.png",
+			"http://manijshrestha.github.io/iPhone-icon.png", "http://manijshrestha.github.io/windows-icon.png" });
+
+	private int index = 0;
+
+	private TextView mTextView;
+	
+	private ViewFlipper mViewFlipper;
+
+	private Button mNextButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_flipper);
+		mTextView = (TextView) findViewById(R.id.title);
+		mViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+		mNextButton = (Button) findViewById(R.id.nextBtn);
+
+		mNextButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				ImageView image = new ImageView(getApplicationContext());
+				UrlImageViewHelper.setUrlDrawable(image, getNextImage());
+				mTextView.setText("Showing: " + index);
+				mViewFlipper.addView(image);
+				mViewFlipper.showNext();
+			}
+		});
+	}
+
+	protected String getNextImage() {
+		if (index == imageURLs.size())
+			index = 0;
+		return imageURLs.get(index++);
+	}
+}
